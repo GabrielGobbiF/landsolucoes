5bc412e97c07298d8744c96193ae6b32ed80257b
gabrielgobbi <icloud.gobbi@gmail.com>
2020-09-24 16:19:33 -0300

    autorização

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,7 @@ Route::prefix('app')->group(function () {
         })->middleware('auth');
     });
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
