985bd928d7cd3cdb707177efe8830f4a6cbc6200
gabrielgobbi <icloud.gobbi@gmail.com>
2020-09-24 16:10:56 -0300

    primeiro commit

---
+++ app/Http/Controllers/PermissionController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Permission;
+use App\Models\Role;
+use App\Models\User;
+
+class PermissionController extends Controller
+{
+    public function permission(Request $request)
+    {
+
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Auth::routes();
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('app')->group(function () {
+    Route::group(['middleware' => 'role:financeiro'], function () {
+        Route::get('/financeiro', function () {
+            return 'Welcome Financeiro';
+        })->middleware('auth');
+    });
+});
