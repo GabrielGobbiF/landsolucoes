86e05ca4f7e5096b6dac567f3649a1d696505c47
gabrielgobbi <icloud.gobbi@gmail.com>
2021-05-12 10:15:38 -0300

    ok

--- resources/views/pages/painel/vehicles/vehicles/show.blade.php
+++ resources/views/pages/painel/vehicles/vehicles/show.blade.php
@@ -18,19 +18,15 @@
                         <div class="col-md-12 mt-2">
                             <div class="card">
                                 <div class="card-body text-center">
-                                    @if (App::environment('production'))
 
-                                        @php$veiId = $vehicle->id;@endphp
+                                    @php
+                                        $veiId = $vehicle->id;
+                                    @endphp
 
-
-
-                                        <img src="data:image/png;base64, {!! base64_encode(
+                                    <img src="data:image/png;base64, {!! base64_encode(
     QrCode::format('png')->size(150)->generate("app.landsolucoes.com.br/v/$veiId/qr"),
 ) !!} ">
 
-                                    @else
-                                        {!! QrCode::size(150)->generate('ItSolutionStuff.com') !!}
-                                    @endif
 
                                     <div class="mt-2">
                                         {{ $vehicle->board }}
