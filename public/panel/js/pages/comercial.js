b4a895cf49a473ff34790a470b10b1042335fb46
gabrielgobbi <icloud.gobbi@gmail.com>
2022-03-17 19:59:21 -0300

    ok

--- public/panel/js/pages/comercial.js
+++ public/panel/js/pages/comercial.js
@@ -35,7 +35,6 @@
         var valorDesconto = clearNumber(vD.val());
 
         total = (parseFloat(valorProposta) - parseFloat(valorDesconto));
-
         vN.val(numberFormat(total));
     }
 
@@ -134,9 +133,39 @@
     }
 
     function clearNumber(number) {
-        number = number.toString().replace("R$", "").replace(".", "");
+        number = number.toString().replace("R$", "").replace(".", "").replace(".", "").replace(".", "");
         number = number.replace(",", ".");
         return number != '' ? numeral(number).value() : 0;
+    }
+
+    function number_format(number, decimals, dec_point, thousands_point) {
+
+        if (number == null || !isFinite(number)) {
+            throw new TypeError("number is not valid");
+        }
+
+        if (!decimals) {
+            var len = number.toString().split('.').length;
+            decimals = len > 1 ? len : 0;
+        }
+
+        if (!dec_point) {
+            dec_point = '.';
+        }
+
+        if (!thousands_point) {
+            thousands_point = ',';
+        }
+
+        number = parseFloat(number).toFixed(decimals);
+
+        number = number.replace(".", dec_point);
+
+        var splitNum = number.split(dec_point);
+        splitNum[0] = splitNum[0].replace(/\B(?=(\d{3})+(?!\d))/g, thousands_point);
+        number = splitNum.join(dec_point);
+
+        return number;
     }
 
 })(jQuery)
