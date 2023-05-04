3582cc92f9b50ab6e1432c2b6ef4217bac604b0b
gabriel gobbi <gabriel.gobbi15@gmail.com>
2023-05-04 13:46:13 -0300

    a

--- resources/views/pages/painel/obras/comercial/show.blade.php
+++ resources/views/pages/painel/obras/comercial/show.blade.php
@@ -444,18 +444,19 @@
                         }
                         $('#row-table-historico').html(options);
 
-                        function btn_delete(v) {
-                var href = $(v).attr('data-href');
-                var title = $(v).attr('data-title');
-                var text = $(v).attr('data-original-title');
-                if (text != null && title != null) {
-                    $('.modal-title').html(title);
-                    $('#modal-confirm').html(text);
-                    $('.modal-text-body').html('Tem certeza que deseja ' + text + '?');
-                }
-                $('#form-modal-action').attr('action', href)
-                $('#modal-delete').modal('show');
-            }
+                        $('.btn-delete').on('click', function(v){
+                            var href = $(v).attr('data-href');
+                            var title = $(v).attr('data-title');
+                            var text = $(v).attr('data-original-title');
+                            if (text != null && title != null) {
+                                $('.modal-title').html(title);
+                                $('#modal-confirm').html(text);
+                                $('.modal-text-body').html('Tem certeza que deseja ' + text + '?');
+                            }
+                            $('#form-modal-action').attr('action', href)
+                            $('#modal-delete').modal('show');
+                        })
+
                     },
                 });
             }
