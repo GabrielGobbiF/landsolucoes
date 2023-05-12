7040dbcdc58e1b572b029a640e17ff7dc349b235
gabriel gobbi <gabriel.gobbi15@gmail.com>
2023-05-12 09:34:12 -0300

    a

--- resources/views/pages/enel/sesmt.blade.php
+++ resources/views/pages/enel/sesmt.blade.php
@@ -2,8 +2,8 @@
 
 <div class="">
     <video width="" controls>
-        <source src="{{asset('panel/videos/WhatsApp Video 2023-05-08 at 12.04.14.mp4')}}" type="video/mp4">
-        <source src="{{asset('panel/videos/WhatsApp Video 2023-05-08 at 12.04.14.mp4')}}" type="video/ogg">
+        <source src="{{asset('panel/videos/smt.mp4')}}" type="video/mp4">
+        <source src="{{asset('panel/videos/smt.mp4')}}" type="video/ogg">
         Your browser does not support HTML video.
     </video>
 </div>
