02b239666543ed5a97f5de5dad9d24769dd4ca72
gabriel gobbi <gabriel.gobbi15@gmail.com>
2024-08-30 09:12:14 -0300

    a

--- resources/views/pages/painel/rdse/rdse/show.blade.php
+++ resources/views/pages/painel/rdse/rdse/show.blade.php
@@ -347,7 +347,7 @@
                                             </div>
 
                                             <div class="mb-3">
-                                                <label class="form-label">Liberado Medição</label>
+                                                <label class="form-label">Data de Pagamento Projeto</label>
                                                 <input type="text" class="form-control form-control-sm input-update date" name="data_pagamento_projeto"
                                                 value="{{ !empty($rdse->data_pagamento_projeto) ? return_format_date($rdse->data_pagamento_projeto, 'pt') : '' }}">
                                             </div>
