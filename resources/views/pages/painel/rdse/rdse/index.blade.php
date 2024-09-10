24ff4c75a34564ce3149df31c01237702fb9122f
gabriel gobbi <gabriel.gobbi15@gmail.com>
2024-09-10 13:25:46 -0300

    a

--- resources/views/pages/painel/rdse/rdse/index.blade.php
+++ resources/views/pages/painel/rdse/rdse/index.blade.php
@@ -435,6 +435,19 @@
 
                             valorTotal += parseFloat(value.valor)
                             valorUpsTotal += parseFloat(value.ups)
+
+                            console.log([
+                                value.total_p1,
+                                value.total_p2,
+                                value.total_p3,
+                                value.valor,
+                                value.ups,
+                                valorTotal,
+                                valorUpsTotal,
+
+                                parseFloat(value.ups),
+
+                            ])
                         });
 
                         $('#totalP1').val(valorP1.toLocaleString('pt-br', {
