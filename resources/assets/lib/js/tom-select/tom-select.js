9bd427207e23ac6c681adcbb9d3fd6d5179e5cf4
gabriel gobbi <gabriel.gobbi15@gmail.com>
2024-11-01 18:52:37 -0300

    a

--- resources/assets/lib/js/tom-select/tom-select.js
+++ resources/assets/lib/js/tom-select/tom-select.js
@@ -27,6 +27,7 @@ const selectAxOption = (request, valueField) => {
 }
 
 window.newSelect = (item) => {
+    let i_url = document.querySelector('meta[ref="url"]').content
     let createIn = item.dataset.create ? true : false;
     let request = item.dataset.request;
     let valueField = item.dataset.valueField ?? 'id';
